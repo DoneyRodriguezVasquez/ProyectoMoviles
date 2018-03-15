95c282927c26a73950e0dbcec7f36e9fff3e98cb
MolinaC4 <alemelemoli21@gmail.com>
2018-03-15 12:43:58 -0600

    Version 0
    0.1

---
+++ app/src/main/java/com/restaurantemeserorapido/alemolina/meserorapido/pantallaprincipal.java
@@ -0,0 +1,13 @@
+package com.restaurantemeserorapido.alemolina.meserorapido;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class pantallaprincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantallaprincipal);
+    }
+}
